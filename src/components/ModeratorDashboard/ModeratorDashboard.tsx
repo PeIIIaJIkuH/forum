31a589d81bceab24eccd0f13a1664876e7d1a075
PeIIIaJIkuH <baigal.erhan@gmail.com>
2022-03-02 09:21:22 +0600

    added lazy load

--- src/components/ModeratorDashboard/ModeratorDashboard.tsx
+++ src/components/ModeratorDashboard/ModeratorDashboard.tsx
@@ -1,4 +1,4 @@
-import {DeleteOutlined} from '@ant-design/icons'
+import {DeleteOutlined, LoadingOutlined} from '@ant-design/icons'
 import {EUserRole} from '../../types'
 import {Error403} from '../common/errors/Error403'
 import {FC} from 'react'
@@ -10,7 +10,7 @@ import {moderatorAPI} from '../../api/moderator'
 import moderatorState from '../../store/moderatorState'
 import {observer} from 'mobx-react-lite'
 import s from './ModeratorDashboard.module.css'
-import {Button, Card, Empty, message} from 'antd'
+import {Button, Card, Empty, message, Spin} from 'antd'
 
 export const ModeratorDashboard: FC = observer(() => {
 	if (!authState.user || authState.role !== EUserRole.moderator) {
@@ -35,26 +35,28 @@ export const ModeratorDashboard: FC = observer(() => {
 				<title>Moderator Dashboard | forume</title>
 			</Helmet>
 			<h2>Reports</h2>
-			{moderatorState.reports.length ? (
-				<div className={s.grid}>
-					{moderatorState.reports.map(({id, postTitle, postId}) => (
-						<div key={id} className={s.card}>
-							<Link to={`/post/${postId}`}>
-								{postTitle}
-							</Link>
-							<div>
-								<Button icon={<DeleteOutlined/>} danger className={s.close} type='link'
-									onClick={onDelete.bind(null, id)} loading={appState.isLoading}
-								/>
+			<Spin spinning={appState.isLoading} indicator={<LoadingOutlined className={s.spinnerIcon}/>}>
+				{moderatorState.reports.length ? (
+					<div className={s.grid}>
+						{moderatorState.reports.map(({id, postTitle, postId}) => (
+							<div key={id} className={s.card}>
+								<Link to={`/post/${postId}`}>
+									{postTitle}
+								</Link>
+								<div>
+									<Button icon={<DeleteOutlined/>} danger className={s.close} type='link'
+										onClick={onDelete.bind(null, id)} loading={appState.isLoading}
+									/>
+								</div>
 							</div>
-						</div>
-					))}
-				</div>
-			) : (
-				<Card>
-					<Empty className={s.empty} description='No Reports'/>
-				</Card>
-			)}
+						))}
+					</div>
+				) : (
+					<Card>
+						<Empty className={s.empty} description='No Reports'/>
+					</Card>
+				)}
+			</Spin>
 		</>
 	)
 })
